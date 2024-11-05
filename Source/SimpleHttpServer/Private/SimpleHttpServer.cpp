e00d9f9f82fe4298c1259512624bbc0f83fb2a04
Mikhail Davydov <davidovmixa0@gmail.com>
2024-11-05 15:02:54 +0300

    Fix for UE 5.4

--- Source/SimpleHttpServer/Private/SimpleHttpServer.cpp
+++ Source/SimpleHttpServer/Private/SimpleHttpServer.cpp
@@ -69,12 +69,15 @@ void USimpleHttpServer::BindRoute(FString HttpPath, ENativeHttpServerRequestVerb
 		FHttpRouteHandle HttpRouteHandle = HttpRouter->BindRoute(FHttpPath(HttpPath), (EHttpServerRequestVerbs)Verbs,
 #if UE_VERSION_OLDER_THAN(5, 4, 0)
 			[&, HttpPath](const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete)
-#else
-			FHttpRequestHandler::CreateLambda([&, HttpPath](const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete)
-#endif
 			{
 				return HandleRequest(HttpPath, Request, OnComplete);
 			});
+#else
+			FHttpRequestHandler::CreateLambda([&, HttpPath](const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete)
+			{
+				return HandleRequest(HttpPath, Request, OnComplete);
+			}));
+#endif
 
 		CreatedRouteHandlers.Add(HttpRouteHandle);
 	}
@@ -93,12 +96,15 @@ void USimpleHttpServer::BindRouteNative(FString HttpPath, ENativeHttpServerReque
 		FHttpRouteHandle HttpRouteHandle = HttpRouter->BindRoute(FHttpPath(HttpPath), (EHttpServerRequestVerbs)Verbs,
 #if UE_VERSION_OLDER_THAN(5, 4, 0)
 			[&, HttpPath](const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete)
-#else
-			FHttpRequestHandler::CreateLambda([&, HttpPath](const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete)
-#endif
 			{
 				return HandleRequestNative(HttpPath, Request, OnComplete);
 			});
+#else
+			FHttpRequestHandler::CreateLambda([&, HttpPath](const FHttpServerRequest& Request, const FHttpResultCallback& OnComplete)
+			{
+				return HandleRequestNative(HttpPath, Request, OnComplete);
+			}));
+#endif
 
 		CreatedRouteHandlers.Add(HttpRouteHandle);
 	}
